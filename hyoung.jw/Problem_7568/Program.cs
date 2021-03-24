11b310dfffb54cd1e5bc6c9f6c30ccef37b7cb44
HYEONG <hyoung.jw@gmail.com>
2021-03-24 23:23:23 +0100

    문제 7568 풀이 내용 작성

--- hyoung.jw/Problem_7568/Program.cs
+++ hyoung.jw/Problem_7568/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Problem_7568
 {
@@ -10,6 +6,29 @@ namespace Problem_7568
     {
         static void Main(string[] args)
         {
+            int person = int.Parse(Console.ReadLine());
+            int[] weight = new int[person];
+            int[] height = new int[person];
+
+            for (int i = 0; i < person; i++)
+            {
+                string input = Console.ReadLine();
+                weight[i] = int.Parse(input.Split(' ')[0]);
+                height[i] = int.Parse(input.Split(' ')[1]);
+            }
+
+            int rank = 1;
+
+            for (int i = 0; i < person; i++)
+            {
+                rank = 1;
+                for (int j = 0; j < person; j++)
+                {
+                    if (weight[i] < weight[j] && height[i] < height[j])
+                        rank++;
+                }
+                Console.Write(rank + " ");
+            }
         }
     }
 }
