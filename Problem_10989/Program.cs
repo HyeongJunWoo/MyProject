5c94da2856a04e2f872a2abef09c845ad8b0ed17
HYEONG <hyoung.jw@gmail.com>
2021-04-06 15:39:32 +0200

    3월 마지막주 문제 제출

---
+++ Problem_10989/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace Problem_10989
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            ArrayList list = new ArrayList();
+            //int[] list = new int[10000];
+            for(int i = 0; i < N; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+            list.Sort();
+
+            StreamWriter sw = new StreamWriter("test.txt");
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                sw.WriteLine(list[i]);
+            }
+
+            sw.Close();
+
+            StreamReader sr = new StreamReader("test.txt");
+
+            Console.WriteLine(sr.ReadToEnd());
+
+            sr.Close();
+        }
+    }
+}
